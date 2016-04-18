ab520ee78ef536775135be05bef999b7f06c48e4
Winlang <rowen0820@gmail.com>
2016-04-18 16:49:46 +0800

    fix

---
+++ script/index.js
@@ -0,0 +1,14 @@
+$(function() {
+	$.post('http://testb.xieli.cn/index.php?s=/api/index/?callback=?',{},function(data){
+        // var data_list = {
+        //     title: '标签',
+        //     list: ['文艺', '博客', '摄影', '电影', '民谣', '旅行', '吉他']
+        // };
+        var data = JSON.parse(data);
+        //alert(typeof(data));
+        var html = template('test', data);
+        document.getElementById('aui-content').innerHTML = html;
+	});
+})
+
+
