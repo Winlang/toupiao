dcb36299b53d267c399177ee7ed3095aee683ef2
asdfzh <1058473569@qq.com>
2016-04-26 16:33:51 +0800

    找活动
    找活动

--- script/catedata.js
+++ script/catedata.js
@@ -1,65 +1,18 @@
-//投票方法
-function toupiao(obj,id){
-	$.post(ApiUrl+'/api/toupiao?id='+id+'&callback=?',{},function(data){
-		var data = JSON.parse(data);
-        if(data.status == 0){
-            alert(data.data);
-            $(obj).removeAttr('onclick');
-            $(obj).children('a').html("已投票");
-        }else{
-            alert(data.data);
-        }
-    });
-}
-
-//发表评论
-function pinglun(id){
-	var comment = '123';
-	var member_id = 1;
-	$.post(ApiUrl+'/api/item_comment?id='+id+'&comment='+comment+'&member_id='+member_id+'&callback=?',{},function(data){
-			var data = JSON.parse(data);
-			if(data.status == 0){
-			    alert(data.data);
-		  	}else{
-			    alert(data.data);
-			}
-	});
-}
-
-//点赞和点扯ajax
-function good_bad(obj,commid,status){
-    $.post(ApiUrl+'/api/comm_goodbad?id='+commid+'&status='+status+'&callback=?',{},function(data){
-    	var data = JSON.parse(data);
-        if(data.status == 0){
-            alert(data.data);
-            $(obj).removeAttr('onclick');
-            if(status == 'good'){
-            	$(obj).next('span').html("已点赞");
-            }else{
-            	$(obj).next('span').html("已点扯");
-            }
-        }else{
-            alert(data.data);
-        }
-    });
-}
 $(function() {
-    //获取主题选项id
-    var itemoptid = getQueryString('itemoptid');
+    
+    var cateid=getQueryString('cateid');
+// alert(cateid);return false;
 
-    //主题选项基本信息
-	$.post(ApiUrl+'/api/itemoption?id='+itemoptid+'&callback=?',{},function(data){
-		var data = JSON.parse(data);
-		$('.aui-title').html(data.data.item_opttitle);
-		var html = template('itemoptdata', data);
-		document.getElementById('itemoptinfo').innerHTML = html;
-	});
-	
-	//主题选项评论列表
-	$.post(ApiUrl+'/api/optioncomments?id='+itemoptid+'&callback=?',{},function(data){
-		var data = JSON.parse(data);
-		
-		var html = template('itemopt_commdata', data);
-		document.getElementById('itemopt_comminfo').innerHTML = html;
-	});
+    $.post(ApiUrl+'/api/cateitems?id='+cateid+'&callback=?',{},function(data){
+
+                var data = JSON.parse(data);
+
+                // 处理图片
+               for (var i = data.list.length - 1; i >= 0; i--) {
+                       data.list[i].item_titleimg = set_item_titleimg(data.list[i].item_titleimg);
+               }
+
+                var html = template('test', data);
+                document.getElementById('aui-content').innerHTML = html;
+        });
 })
