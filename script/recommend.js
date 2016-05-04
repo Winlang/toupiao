2950cd84b6d3ed58d89f7e7deafe015ec4b07475
asdfzh <1058473569@qq.com>
2016-05-04 15:31:29 +0800

    推荐

--- script/recommend.js
+++ script/recommend.js
@@ -1,13 +1,33 @@
 $(function() {
     
     
-
+       var item_id;
+       var member_id=is_login();
 
     $.post(ApiUrl+'/api/recommend_item/?callback=?',{},function(data){
-      alert(data);return false;
                 var data = JSON.parse(data);
-                var html = template('itemoptdata', data);
+                data.data.item_titleimg = set_item_titleimg(data.data.item_titleimg);
 
-                document.getElementById('aui-content').innerHTML = html;
+                var html = template('item', data);
+
+                item_id=data.data.id;
+                document.getElementById('iteminfo').innerHTML = html;
+
+                //主题对应选项列表
+
+        $.post(ApiUrl+'/api/itemoptions?id='+item_id+'&member_id='+member_id+'&callback=?',{},function(res){
+              
+                var data = JSON.parse(res);
+                
+                //处理图片
+                for (var i = data.data.length - 1; i >= 0; i--) {
+                        data.data[i].item_optimg = set_item_optimg(data.data[i].item_optimg);
+                }
+
+                //alert(typeof(data));
+                var html = template('itemoptions', data);
+                document.getElementById('itemoption_list').innerHTML = html;
         });
+        });
+     
 })
