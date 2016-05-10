f160c36aa91ce68eaab7bc08a11380b695813236
z843433489 <843433489@qq.com>
2016-05-10 10:06:20 +0800

    平路你取消点赞和点扯
    平路你取消点赞和点扯

--- script/vote.js
+++ script/vote.js
@@ -78,6 +78,9 @@ function pinglun(){
 
 //点赞和点扯ajax
 function good_bad(obj,commid,status){
+	alert(commid);
+	alert(status);
+	return false;
 	var member_id = is_login();
 	if(member_id == '-1'){
 		api.alert({msg: '请先登录'});
@@ -88,7 +91,7 @@ function good_bad(obj,commid,status){
     	var data = JSON.parse(data);
         if(data.status == 0){
             api.alert({msg: data.data});
-            $(obj).removeAttr('onclick');
+            $(obj).attr('onclick','cancel_goodbad('+obj+','+data.comm_id+')');
         	$(obj).removeClass('aui-icon-appreciate');
         	$(obj).addClass('aui-icon-appreciatefill');
         	$(obj).addClass('aui-text-warning');
@@ -97,6 +100,32 @@ function good_bad(obj,commid,status){
             api.alert({msg: data.data});
         }
     });
+}
+
+//取消点赞点扯
+function cancel_goodbad(obj,comm_id){
+	var member_id = is_login();
+	if(member_id == '-1'){
+		api.alert({msg: '请先登录'});
+		login_page();
+		return false;
+	}
+
+	//请求接口
+	$.post(ApiUrl+'/api/cancel_goodbad?comm_id='+commid+'&member_id='+member_id+'&callback=?',{},function(data){
+    	var data = JSON.parse(data);
+        if(data.status == 0){
+            api.alert({msg: data.data});
+            $(obj).attr('onclick','good_bad('+obj+','+data.comm_id+','+data.good_bad+')');
+        	$(obj).removeClass('aui-icon-appreciate');
+        	$(obj).addClass('aui-icon-appreciatefill');
+        	$(obj).addClass('aui-text-warning');
+        	$(obj).next('span').html(data.num);
+        }else{
+            api.alert({msg: data.data});
+        }
+    });
+
 }
 
 //点击加载更多最新评论
